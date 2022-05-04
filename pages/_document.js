05afb9018b6243d98d54e2c284548dabe67faae1
MorganeT84 <morganetressens@gmail.com>
2022-05-04 08:52:19 +0200

    ajout de link fontawesome prise en charge autre navigateur

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Economica:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-
+          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
         </Head>
         <body>
           <Main />
