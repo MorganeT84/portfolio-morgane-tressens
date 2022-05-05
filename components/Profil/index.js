d6552ba7699289ff1abb469bd12ef71cec38a0bb
MorganeT84 <morganetressens@gmail.com>
2022-05-05 18:57:35 +0200

    profil softskill

--- components/Profil/index.js
+++ components/Profil/index.js
@@ -8,10 +8,16 @@ const Profil = () => (
       <div className={styles.bloc}>
         <h2> Une formation</h2>
         <p>
-          Le trente septembre deux mille vingt, j'investi dans un nouvel ordinateur pour entreprendre une nouvelle carrière professionnelle. Après quelques tutorielles de code pour découvrir et créer ma première page web, j’entreprends les démarches pour intégrer une formation avec l'école O’clock . <br />
+          Le trente septembre deux mille vingt, j'investis dans un nouvel ordinateur pour entreprendre une nouvelle carrière professionnelle. Après quelques tutorielles de code pour découvrir et créer ma première page web, j’entreprends les démarches pour intégrer une formation avec l'école O’clock . <br />
           Auxiliaire vétérinaire depuis de 14 ans, j’ai commencé à scruter les codes sources des sites que je consultais dans le cadre de mon travail. Le 7 Juin 2021, 9 a.m.,  j’embarque à bord de la promotion Valkyrie pour 24 semaines intenses dans l’apprentissage du développement web.
         </p>
       </div>
+      <div className={styles.bloc}>
+        <h2> Softskill</h2>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </div> 
       <div className={styles.bloc}>
         <h3>Mes compétences</h3>
         <p>
